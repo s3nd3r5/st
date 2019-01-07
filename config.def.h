5a2b210a7b8e11ba4572fb13e30ba77c41c67de5
Stephen Enders <senders@spotify.com>
2019-01-07 13:47:01 -0500

    Added shift+insert as paste

--- config.def.h
+++ config.def.h
@@ -201,6 +201,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ShiftMask, 		XK_Insert,	clippaste,	{.i = 0} },
 };
 
 /*
