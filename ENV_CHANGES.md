656f5df65b7257ab358be3b062f6b9569cccd2a3
Stephen Enders <senders@spotify.com>
2019-02-09 12:55:45 -0500

    Changed font to Luxi Mono, included md file with notes on some external changes that need to be done if running this branch

---
+++ ENV_CHANGES.md
@@ -0,0 +1,9 @@
+Environtment Changes
+====================
+
+When usign st-term alias: `ssh` to `TERM=xterm-256color; ssh` to allow for remote shells to work properly
+
+Fonts
+=====
+
+Currently the font is set to: `Luxi Mono` which can be downloaded from: [Font Squirrel](https://www.fontsquirrel.com/fonts/Luxi-Mono)
--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+// static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Luxi Mono:style=Regular:size=9:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
